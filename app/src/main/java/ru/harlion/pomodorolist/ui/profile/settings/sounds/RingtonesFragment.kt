52446cfb656e4419554a793167f2bd9fad4cb1e9
Shulzhenko <sasiskajik@mail.ru>
2022-03-02 13:36:04 +0300

    adapter sound

--- app/src/main/java/ru/harlion/pomodorolist/ui/profile/settings/sounds/RingtonesFragment.kt
+++ app/src/main/java/ru/harlion/pomodorolist/ui/profile/settings/sounds/RingtonesFragment.kt
@@ -1,6 +1,10 @@
 package ru.harlion.pomodorolist.ui.profile.settings.sounds
 
 
+import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import ru.harlion.pomodorolist.AppActivity
 import ru.harlion.pomodorolist.R
 import ru.harlion.pomodorolist.base.BindingFragment
 import ru.harlion.pomodorolist.databinding.FragmentRingtonsBinding
@@ -8,12 +12,41 @@ import ru.harlion.pomodorolist.databinding.FragmentRingtonsBinding
 
 class RingtonesFragment : BindingFragment<FragmentRingtonsBinding>(FragmentRingtonsBinding::inflate) {
 
+    private lateinit var adapterSound: SoundsAdapter
 
-   private fun listSound() = listOf(
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initRecyclerSound()
+    }
+
+    private fun initRecyclerSound() {
+        val llm = LinearLayoutManager(requireContext())
+        llm.orientation = LinearLayoutManager.VERTICAL
+        adapterSound = SoundsAdapter {  }
+        binding.ringtonesRecyclerView.apply {
+            layoutManager = llm
+            adapter = adapterSound
+        }
+
+        adapterSound.items = listSound()
+    }
+
+    private fun listSound() = listOf(
        Sound(1, getString(R.string.no) , null),
        Sound(2, getString(R.string.tick_sound) , R.raw.alarm_clock),
        Sound(3, getString(R.string.sound_bird) , R.raw.sound_birds),
        Sound(4, getString(R.string.sound_fire) , R.raw.sound_fire),
        Sound(5, getString(R.string.sound_water) , R.raw.sound_water),
    )
+
+    override fun onStart() {
+        super.onStart()
+        (activity as AppActivity).setBottomNavigationVisible(false)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        (activity as AppActivity).setBottomNavigationVisible(true)
+    }
 }
--- app/src/main/java/ru/harlion/pomodorolist/ui/profile/settings/sounds/SoundsAdapter.kt
+++ app/src/main/java/ru/harlion/pomodorolist/ui/profile/settings/sounds/SoundsAdapter.kt
@@ -1,10 +1,40 @@
 package ru.harlion.pomodorolist.ui.profile.settings.sounds
 
-class SoundsAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ru.harlion.pomodorolist.base.BindingHolder
+import ru.harlion.pomodorolist.databinding.ItemSoundBinding
+
+
+private typealias ItemHolderSound = BindingHolder<ItemSoundBinding>
+
+class SoundsAdapter(private val clickDone: (Long) -> Unit) :
+RecyclerView.Adapter<ItemHolderSound>(){
+
+    var items: List <Sound> = listOf()
+    set (value) {
+        field = value
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        ItemHolderSound(ItemSoundBinding::inflate, parent).apply {
+
+    }
+
+    override fun onBindViewHolder(holder: ItemHolderSound, position: Int) {
+       holder.binding.apply {
+           textSound.text = items[position].name
+           textSound.isChecked = items[position].isChecked
+       }
+    }
+
+    override fun getItemCount() = items.size
 }
 
 class Sound(
-    val id : Int,
+    val id : Long,
     val name: String,
-    val rawId: Int?
+    val rawId: Int?,
+    val isChecked : Boolean = false
 )
